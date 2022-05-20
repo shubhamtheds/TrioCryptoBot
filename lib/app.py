84e5cb7781a8ca9bf4a7c2ee702e58976c6d7722
Shubham Kumar Sharma <89303017+shubhamtheds@users.noreply.github.com>
2022-05-20 18:32:51 +0000

    modify app.py

--- lib/app.py
+++ lib/app.py
@@ -3,12 +3,11 @@ import os
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
-import pandas as pd
-import pprint 
 
-url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
-  'slug' : 'bitcoin',
+  'start' : '1',
+  'limit' : '5',
   'convert':'INR'
 }
 headers = {
@@ -22,16 +21,57 @@ session.headers.update(headers)
 try:
   response = session.get(url, params=parameters)
   data = json.loads(response.text)
+
+  #BTC DATA
+  btc_data_inr = data['data'][0]['quote']['INR']
   
-  price = data['data']['1']['quote']['INR']['price']
-  vol_ch_24h = data['data']['1']['quote']['INR']['volume_change_24h']
-  per_ch_30d = data['data']['1']['quote']['INR']['percent_change_30d']
-  market_cap = data['data']['1']['quote']['INR']['market_cap']
+  btc_price = btc_data_inr['price']
+  
+  btc_vol_ch_24h = btc_data_inr['volume_change_24h']
+  
+  btc_per_ch_30d = btc_data_inr['percent_change_30d']
+  
+  btc_market_cap = btc_data_inr['market_cap']
+
+  eth_price = data['data'][1]['quote']['INR']['price']
+  
+  eth_vol_ch_24h = data['data'][1]['quote']['INR']['volume_change_24h']
+  
+  eth_per_ch_30d = data['data'][1]['quote']['INR'] ['percent_change_30d']
+  
+  eth_market_cap = data['data'][1]['quote']['INR']['market_cap']
+
+  usdt_price = data['data'][2]['quote']['INR']['price']
+  
+  usdt_vol_ch_24h = data['data'][2]['quote']['INR']['volume_change_24h']
+  
+  usdt_per_ch_30d = data['data'][2]['quote']['INR'] ['percent_change_30d']
+  
+  usdt_market_cap = data['data'][2]['quote']['INR']['market_cap']
+
+  usdc_price = data['data'][3]['quote']['INR']['price']
+  
+  usdc_vol_ch_24h = data['data'][3]['quote']['INR']['volume_change_24h']
+  
+  usdc_per_ch_30d = data['data'][3]['quote']['INR'] ['percent_change_30d']
+  
+  usdc_market_cap = data['data'][3]['quote']['INR']['market_cap'] 
+
+  bnb_price = data['data'][4]['quote']['INR']['price']
+  
+  bnb_vol_ch_24h = data['data'][4]['quote']['INR']['volume_change_24h']
+  
+  bnb_per_ch_30d = data['data'][4]['quote']['INR'] ['percent_change_30d']
+  
+  bnb_market_cap = data['data'][4]['quote']['INR']['market_cap']
+
+
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
 
 
-# df2 = json_normalize(data) 
-# print(df2)
+
+
+
 
 
--- main.py
+++ main.py
@@ -1,33 +1,99 @@
 import os
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
-from lib.app import price, vol_ch_24h, per_ch_30d, market_cap
+from lib.app import *
+  
 
-
-def hello(update: Update, context: CallbackContext):
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
-def btc_price(update: Update, context: CallbackContext):
-    update.message.reply_text(f'₹{price}')
+def get_btc_price(update: Update, context: CallbackContext):
+    update.message.reply_text(f'₹{btc_price}')
 
 def btc_vol_ch_24h(update: Update, context: CallbackContext):
-    update.message.reply_text(f'{vol_ch_24h}%')
+    update.message.reply_text(f'{btc_vol_ch_24h}%')
   
 def btc_per_ch_30d(update: Update, context: CallbackContext):
-    update.message.reply_text(f'{per_ch_30d}%')
+    update.message.reply_text(f'{btc_per_ch_30d}%')
   
 def btc_market_cap(update: Update, context: CallbackContext):
-    update.message.reply_text(f'₹{market_cap}')
+    update.message.reply_text(f'₹{btc_market_cap}')
+
+def eth_price(update: Update, context: CallbackContext):
+    update.message.reply_text(f'₹{eth_price}')
+
+def eth_vol_ch_24h(update: Update, context: CallbackContext):
+    update.message.reply_text(f'{eth_vol_ch_24h}%')
+  
+def eth_per_ch_30d(update: Update, context: CallbackContext):
+    update.message.reply_text(f'{eth_per_ch_30d}%')
+  
+def eth_market_cap(update: Update, context: CallbackContext):
+    update.message.reply_text(f'₹{eth_market_cap}')
+
+def usdt_price(update: Update, context: CallbackContext):
+    update.message.reply_text(f'₹{usdt_price}')
+
+def usdt_vol_ch_24h(update: Update, context: CallbackContext):
+    update.message.reply_text(f'{usdt_vol_ch_24h}%')
+  
+def usdt_per_ch_30d(update: Update, context: CallbackContext):
+    update.message.reply_text(f'{usdt_per_ch_30d}%')
+  
+def usdt_market_cap(update: Update, context: CallbackContext):
+    update.message.reply_text(f'₹{usdt_market_cap}')
+
+def usdc_price(update: Update, context: CallbackContext):
+    update.message.reply_text(f'₹{usdc_price}')
+
+def usdc_vol_ch_24h(update: Update, context: CallbackContext):
+    update.message.reply_text(f'{usdc_vol_ch_24h}%')
+  
+def usdc_per_ch_30d(update: Update, context: CallbackContext):
+    update.message.reply_text(f'{usdc_per_ch_30d}%')
+  
+def usdc_market_cap(update: Update, context: CallbackContext):
+    update.message.reply_text(f'₹{usdc_market_cap}')
+
+def bnb_price(update: Update, context: CallbackContext):
+    update.message.reply_text(f'₹{bnb_price}')
+
+def bnb_vol_ch_24h(update: Update, context: CallbackContext):
+    update.message.reply_text(f'{bnb_vol_ch_24h}%')
+  
+def bnb_per_ch_30d(update: Update, context: CallbackContext):
+    update.message.reply_text(f'{bnb_per_ch_30d}%')
+  
+def bnb_market_cap(update: Update, context: CallbackContext):
+    update.message.reply_text(f'₹{bnb_market_cap}')
+
   
 updater = Updater(os.environ['TOKEN'])
 
 print("Bot is active! 🚀")
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
-updater.dispatcher.add_handler(CommandHandler('btc_price', btc_price))
+
+updater.dispatcher.add_handler(CommandHandler('btc_price', get_btc_price))
 updater.dispatcher.add_handler(CommandHandler('btc_vol_ch_24h', btc_vol_ch_24h))
 updater.dispatcher.add_handler(CommandHandler('btc_per_ch_30d', btc_per_ch_30d))
 updater.dispatcher.add_handler(CommandHandler('btc_market_cap', btc_market_cap))
+
+updater.dispatcher.add_handler(CommandHandler('eth_price', eth_price))
+updater.dispatcher.add_handler(CommandHandler('eth_vol_ch_24h', eth_vol_ch_24h))
+updater.dispatcher.add_handler(CommandHandler('eth_per_ch_30d', eth_per_ch_30d))
+updater.dispatcher.add_handler(CommandHandler('eth_market_cap', eth_market_cap))
+
+updater.dispatcher.add_handler(CommandHandler('usdt_price', usdt_price))
+updater.dispatcher.add_handler(CommandHandler('usdt_vol_ch_24h', usdt_vol_ch_24h))
+updater.dispatcher.add_handler(CommandHandler('usdt_per_ch_30d', usdt_per_ch_30d))
+updater.dispatcher.add_handler(CommandHandler('usdt_market_cap', usdt_market_cap))
+
+updater.dispatcher.add_handler(CommandHandler('usdc_price', usdc_price))
+updater.dispatcher.add_handler(CommandHandler('usdc_vol_ch_24h', usdc_vol_ch_24h))
+updater.dispatcher.add_handler(CommandHandler('usdc_per_ch_30d', usdc_per_ch_30d))
+updater.dispatcher.add_handler(CommandHandler('usdc_market_cap', usdc_market_cap))
+
+updater.dispatcher.add_handler(CommandHandler('bnb_price', bnb_price))
+updater.dispatcher.add_handler(CommandHandler('bnb_vol_ch_24h', bnb_vol_ch_24h))
+updater.dispatcher.add_handler(CommandHandler('bnb_per_ch_30d', bnb_per_ch_30d))
+updater.dispatcher.add_handler(CommandHandler('bnb_market_cap', bnb_market_cap))
 
 
 updater.start_polling()
