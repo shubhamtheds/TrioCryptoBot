1c342f50861b248bd8154e0f18fbb9380a09d81e
Shubham Kumar Sharma <89303017+shubhamtheds@users.noreply.github.com>
2022-06-18 18:42:03 +0000

    Updated app.py

--- lib/app.py
+++ lib/app.py
@@ -7,7 +7,7 @@ import json
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
   'start' : '1',
-  'limit' : '20',
+  'limit' : '21',
   'convert':'INR'
 }
 headers = {
